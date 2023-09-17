7543c2d359eedadaedf8099e781e381156483920
adrian <adrian@iraizos.me>
2023-09-17 20:18:58 +0200

    Initial Commit

---
+++ CityBus/CityBusApp.swift
@@ -0,0 +1,17 @@
+//
+//  CityBusApp.swift
+//  CityBus
+//
+//  Created by Adrian Iraizos Mendoza on 17/9/23.
+//
+
+import SwiftUI
+
+@main
+struct CityBusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CityBus/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CityBus
+//
+//  Created by Adrian Iraizos Mendoza on 17/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
